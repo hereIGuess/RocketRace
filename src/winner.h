f7ceff439b17676cf024c7f404d83f6bdc761245
Nat M <nmogk@rrc.ca>
2022-11-21 09:32:12 -0600

    Fixed winner text to properly display the winner

--- src/winner.h
+++ src/winner.h
@@ -11,24 +11,33 @@ private:
 	//int player2Score;
 	Point position;
 	std::string winnerText;
+	std::string continueText = "Press SPACE to return to title screen";
 
 public:
+	Winner() {};
+
 	Winner(Score score1) {
 		compareScores(score1);
 	}
 
 	void compareScores(Score score1) {
 		//instead of 1 insert player 2's score
-		if (score1.getScore() > 1) {
-			position = Point{ score1.getPosition().x, score1.getPosition().y + 20 };
+		if (score1.getScore() >= 1) {
+			position = Point{ score1.getPosition().x - 50, score1.getPosition().y + 30 };
 			winnerText = "Player 1 wins!";
 		} else if (score1.getScore() < 1) {
-			position = Point{ score1.getPosition().x, score1.getPosition().y + 20 };
+			position = Point{ score1.getPosition().x - 50, score1.getPosition().y + 30 };
 			winnerText = "Player 2 wins!";
 		}
 	}
 
 	void render() {
 		ofDrawBitmapString(winnerText, position.x, position.y);
+
+		ofSetColor(150);
+		ofDrawRectangle(screen.x / 5 - 10, screen.y / 2 - 20, 320, 30);
+
+		ofSetColor(255);
+		ofDrawBitmapString(continueText, screen.x / 5, screen.y / 2);
 	}
 };
